8cd613ef6b69b56b604c1601ea766abbeff4ba0b
Fabricio <122045463+FabricioFacco@users.noreply.github.com>
2023-05-15 16:49:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-window.scroll({
-  top: 0,
-  behavior: 'smooth',
-})
+const links = document.querySelectorAll('a[href^="#"]');
+
+links.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+
+    const href = link.getAttribute('href');
+    const target = document.querySelector(href);
+
+    target.scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
